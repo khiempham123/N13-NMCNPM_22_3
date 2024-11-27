c4fb69a1f690a715d4cce47bdc7d508ce8a92328
dnquocdat <dangngocquocdat10112004@gmail.com>
2024-11-27 23:33:39 +0700

    sua detail

--- src/book-management-system/index.js
+++ src/book-management-system/index.js
@@ -1,23 +1,21 @@
-const express = require('express');
-const cors = require('cors'); // Import thư viện CORS
-require('dotenv').config();
-const route = require('./routes/client/index.route');
-const { connect } = require('./config/database'); // Import hàm connect từ database.js
-
-
+const express = require("express");
+const cors = require("cors"); // Import thư viện CORS
+require("dotenv").config();
+const route = require("./routes/client/index.route");
+const { connect } = require("./config/database"); // Import hàm connect từ database.js
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.static("public"));
 // cấu hình cors
-app.use(cors({
-  origin: 'http://127.0.0.1:5501',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
-
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5500",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 // Kết nối tới MongoDB
 connect();
