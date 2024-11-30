5f79bd9b85dc2b82fae3d6996c8caabb59a74d62
PhamGiaKhiem <pgk06052018@gmail.com>
2024-11-30 10:22:52 +0700

    full handle click

--- src/book-management-system/views/client/pages/market/payment.js
+++ src/book-management-system/views/client/pages/market/payment.js
@@ -6,27 +6,35 @@ document.addEventListener("DOMContentLoaded", function () {
     const checkoutBtn = document.getElementById("checkoutBtn");
     const paymentSection = document.querySelector(".payment-section");
     const deliverySection = document.querySelector(".delivery-section");
-    const btnContinueToDelivery = document.querySelector(".btn-continue");
-    const btnBackToPayment = document.querySelector(".btn-back");
+    const btnContinueToDelivery = document.getElementById("continueToDelivery");
+    const btnBackToCart = document.getElementById("backToCart");
+    const btnBackToPayment = document.getElementById("backToPayment"); 
     const blurElements = document.querySelectorAll(".blur");
 
     // Hiển thị phần thanh toán
     checkoutBtn.addEventListener("click", function () {
         paymentSection.classList.add("active");
         overlay.classList.add("active");
-        blurElements.forEach((el) => el.classList.add("blur"));
+        //blurElements.forEach((el) => el.classList.add("blur"));
     });
 
     // Chuyển đến phần giao hàng
     btnContinueToDelivery.addEventListener("click", function () {
-        paymentSection.classList.remove("active");
         deliverySection.classList.add("active");
+        //blurElements.forEach((el) => el.classList.remove("blur"));
     });
 
     // Quay lại phần thanh toán
+    btnBackToCart.addEventListener("click", function () {
+        paymentSection.classList.remove("active");
+        overlay.classList.remove("active");
+        blurElements.forEach((el) => el.classList.remove("blur"));
+    });
+
     btnBackToPayment.addEventListener("click", function () {
         deliverySection.classList.remove("active");
         paymentSection.classList.add("active");
+        //blurElements.forEach((el) => el.classList.add("blur"));
     });
 
     // Ẩn overlay và các phần modal khi nhấn ra ngoài
