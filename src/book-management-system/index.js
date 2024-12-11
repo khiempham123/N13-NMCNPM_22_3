589591585cd1fcbb1240a96a43a81175b3fd2091
PhamGiaKhiem <pgk06052018@gmail.com>
2024-12-11 09:06:59 +0700

    not fix at now

--- src/book-management-system/index.js
+++ src/book-management-system/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://127.0.0.1:5500",
+    origin: "http://127.0.0.1:5501",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     headers: ["Content-Type", "authorization"],
   })
