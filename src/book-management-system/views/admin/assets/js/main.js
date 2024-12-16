c0dbede32c710a97cab379bca17b27d3f72ef56e
dnquocdat <dangngocquocdat10112004@gmail.com>
2024-12-16 17:46:22 +0700

    sua 1 vai cai nho

---
+++ src/book-management-system/views/admin/assets/js/main.js
@@ -0,0 +1,40 @@
+// add hovered class to selected list item
+let list = document.querySelectorAll(".navigation li");
+
+function activeLink() {
+  list.forEach((item) => {
+    item.classList.remove("hovered");
+  });
+  this.classList.add("hovered");
+}
+
+list.forEach((item) => item.addEventListener("mouseover", activeLink));
+
+// Menu Toggle
+let toggle = document.querySelector(".toggle");
+let navigation = document.querySelector(".navigation");
+let main = document.querySelector(".main");
+
+toggle.onclick = function () {
+  navigation.classList.toggle("active");
+  main.classList.toggle("active");
+};
+
+//Add Product, Customer, Staff
+const addProductModal = document.getElementById("addProductModal");
+const addProductBtn = document.getElementById("btn-addproduct");
+const closeBtn = document.getElementsByClassName("close")[0];
+
+addProductBtn.addEventListener("click", () => {
+  addProductModal.style.display = "block";
+});
+
+closeBtn.addEventListener("click", () => {
+  addProductModal.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+  if (event.target == addProductModal) {
+    addProductModal.style.display = "none";
+  }
+});
